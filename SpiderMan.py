c986a42d6961e22855de25d6fee8c43673783621
wjl <JianLiWen>
2019-11-25 10:55:57 +0800

    运行结果

--- SpiderMan.py
+++ SpiderMan.py
@@ -17,7 +17,7 @@ class SpiderMan(object):
         # 添加入口URL
         self.manager.add_new_url(root_url)
         # 判断url管理器中是否有新的url，同时判断取了多少个url
-        while(self.manager.has_new_url() and self.manager.old_url_size()<10):
+        while(self.manager.has_new_url() and self.manager.old_url_size()<100):
             try:
                 new_url = self.manager.get_new_url()
                 html = self.downloader.download(new_url)
